4186a8f2638933701d92555d487918ba85496465
Will Hohyon Ryu <will@oxopolitics.com>
2025-05-13 17:35:19 +0900

    feat: update Nobel Prize data in economic theories and enhance display in ScholarCard

--- src/app/economy-101/ScholarCard.js
+++ src/app/economy-101/ScholarCard.js
@@ -8,8 +8,8 @@ const ScholarCard = ({ scholar }) => {
   return (
     <div className="border border-gray-200 rounded-lg bg-slate-50 shadow-sm w-40 flex-shrink-0 overflow-hidden relative">
       {scholar.nobelPrize && (
-        <div className="absolute -right-5 top-[6px] w-[80px] transform rotate-45 bg-yellow-500 text-white text-[10px] font-semibold text-center py-px shadow-md">
-          Nobel
+        <div className="absolute -right-5 top-[6px] w-[80px] transform rotate-45 bg-yellow-500 text-white text-[10px] font-semibold text-center py-px shadow-md flex items-center justify-center">
+          {`N ${scholar.nobelPrize}`}
         </div>
       )}
       {scholar.image && (
