163afac572661c5a4e1b232b0b3959640a06208b
Will Hohyon Ryu <will@oxopolitics.com>
2025-05-19 23:30:25 +0900

    feat: Remove unused React import in DigitalEconomyInstitutionsPoster component

--- src/app/digital-economy/economic-institutions/page.js
+++ src/app/digital-economy/economic-institutions/page.js
@@ -1,7 +1,6 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
 import AnimateOnScroll from "../../../components/AnimateOnScroll";
 
 const DigitalEconomyInstitutionsPoster = () => {
